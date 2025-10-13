70ca5497fdba24a3b7bf40ed0cfc4aee449c70e7
Luciano Sciarretta <clsciarretta@hotmail.com>
2025-10-13 18:15:45 -0300

    allowed hosts 3

--- AuditAI/settings.py
+++ AuditAI/settings.py
@@ -6,7 +6,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'audit-ai-fl73.onrender.com']
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
